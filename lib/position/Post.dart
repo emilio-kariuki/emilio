6f420411c34bfb498373568fe731501150a72c48
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-27 19:14:32 +0300

    commit

--- lib/position/Post.dart
+++ lib/position/Post.dart
@@ -56,124 +56,129 @@ class _PostState extends State<Post> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         backgroundColor: Colors.blueGrey[800],
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: SvgPicture.asset(
-                  "assets/svg/login.svg",
-                  height: size.height * 0.09,
-                ),
-              ),
-              Row(
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(4, 0, 0, 0 ),
-                        child: Text("Name", style: GoogleFonts.roboto(fontSize:20,color: Colors.white)),
-                      ),
-              SizedBox(height: 4),
-              SizedBox(
-                height: 50,
-                width:  170,
-                child: TextFormField(
-                  autocorrect: true,
-                  controller: name,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        // icon: Icon(Icons.person,size:30,color: Colors.black),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey[800]),
-                        focusColor: Colors.red,
-                        // prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
-                        // hintText: "Name",
-                        fillColor: Colors.grey[200]),
-                ),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(4, 0, 0, 0 ),
-                child: Text("Categories", style: GoogleFonts.roboto(fontSize:20,color: Colors.white)),
-              ),
-              SizedBox(height: 4),
-              SizedBox(
-                height: 50,
-                width:  170,
-                child: TextFormField(
-                  autocorrect: true,
-                  controller: name,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        // icon: Icon(Icons.person,size:30,color: Colors.black),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey[800]),
-                        focusColor: Colors.red,
-                        // prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
-                        // hintText: "Name",
-                        fillColor: Colors.grey[200]),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(width: 10,),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0,5,0,4),
-            child: SizedBox(
-              height: 140,
-              width: 160,
-              child: Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    SizedBox(height: 4,),
-                    Icon(Icons.location_on,color: Colors.blueGrey[800],size: 40),
-                    Text("Location", style: GoogleFonts.roboto(fontSize:20,color: Colors.blueGrey[800],fontWeight: FontWeight.bold)),
-                    SizedBox(height: 15),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Push", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(Colors.blueGrey[800]),
-                          ),
-                        ),
+                  Center(
+                    child: SvgPicture.asset(
+                      "assets/svg/login.svg",
+                      height: size.height * 0.09,
                     ),
-                  ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(4, 0, 0, 0 ),
+                            child: Text("Name", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
+                          ),
+                  SizedBox(height: 4),
+                  SizedBox(
+                    height: 50,
+                    width:  170,
+                    child: TextFormField(
+                      autocorrect: true,
+                      controller: name,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            // icon: Icon(Icons.person,size:30,color: Colors.black),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            focusColor: Colors.red,
+                            // prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
+                            // hintText: "Name",
+                            fillColor: Colors.grey[200]),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(4, 0, 0, 0 ),
+                    child: Text("Categories", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
+                  ),
+                  SizedBox(height: 4),
+                  SizedBox(
+                    height: 50,
+                    width:  170,
+                    child: TextFormField(
+                      autocorrect: true,
+                      controller: name,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            // icon: Icon(Icons.person,size:30,color: Colors.black),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            focusColor: Colors.red,
+                            // prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
+                            // hintText: "Name",
+                            fillColor: Colors.grey[200]),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(width: 10,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,6,0,4),
+                child: SizedBox(
+                  height: 150,
+                  width: 160,
+                  child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 4,),
+                        Icon(Icons.location_on,color: Colors.blueGrey[800],size: 40),
+                        Text("Location", style: GoogleFonts.roboto(fontSize:20,color: Colors.blueGrey[800],fontWeight: FontWeight.bold)),
+                        SizedBox(height: 15),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Push", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(Colors.blueGrey[800]),
+                              ),
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ],
+            ),
+            SizedBox(height: 5,),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 0,0,0),
+              child: Text("Picture", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+              child: SizedBox(
+                height: size.height * 0.25,
+                width: size.width,
+                child: Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  color: Colors.white,
                 ),
               ),
             ),
+            Text("Quantity", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
+            ],
+                ),
+              ),
           ),
-          ],
-        ),
-        SizedBox(height: 5,),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(5, 0,0,0),
-          child: Text("Picture", style: GoogleFonts.roboto(fontSize:20,color: Colors.white,fontWeight: FontWeight.bold)),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
-          child: SizedBox(
-            height: size.height * 0.25,
-            width: size.width,
-            child: Card(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-              color: Colors.white,
-            ),
-          ),
-        )
-        ],
-      ),
-    ));
+        ));
   }
 }
