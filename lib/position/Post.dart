8881dd7bccc7dccb7a09751fa368e1eec42238c1
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-29 20:31:40 +0300

    commit

--- lib/position/Post.dart
+++ lib/position/Post.dart
@@ -1,15 +1,18 @@
-// ignore_for_file: deprecated_member_use, avoid_print, prefer_final_fields, unused_import, unused_label, dead_code, unnecessary_import
+// ignore_for_file: deprecated_member_use, avoid_print, prefer_final_fields, unused_import, unused_label, dead_code, unnecessary_import, nullable_type_in_catch_clause
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:emilio/position/position.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../Home.dart';
 // import 'package:geolocator/geolocator.dart';
@@ -237,6 +240,27 @@ class _PostState extends State<Post> {
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20)),
                           color: Colors.white,
+                          child: image != null
+              ? ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.file(
+                    image!,
+                    width: 200,
+                    height: 100,
+                    fit: BoxFit.fitHeight,
+                  ),
+                )
+              : Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(50)),
+                  width: 200,
+                  height: 100,
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.grey[800],
+                  ),
+                ),
                         ),
                       ),
                       Positioned(
@@ -244,7 +268,10 @@ class _PostState extends State<Post> {
                         right: 150,
                         child: IconButton(
                           onPressed: (){
-                          }, 
+                            showModalBottomSheet(
+                              context: (context),
+                              builder: ((builder) => bottomSheet(context)));
+                          },
                           icon: Icon(Icons.camera_alt_rounded,size: 30,color: Colors.blueGrey[800],)),)
                       ]
                       ),
@@ -305,7 +332,6 @@ class _PostState extends State<Post> {
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20),
                             ),
-                            
                             child: Text("Submit",
                                 style: GoogleFonts.robotoCondensed(
                                     fontSize: 20, color: Colors.black,fontWeight: FontWeight.bold))),
@@ -328,4 +354,46 @@ class _PostState extends State<Post> {
           this.value = value!;
         });
       });
+
+    Widget bottomSheet(BuildContext context) {
+    return Container(
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Choose an Image from ",
+              style: GoogleFonts.roboto(
+                  fontSize: 24, color: Colors.blueGrey[900])),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton.icon(
+                  onPressed: () {
+                    takePhoto(ImageSource.camera);
+                  },
+                  icon:
+                      Icon(Icons.camera, color: Colors.blueGrey[900], size: 34),
+                  label: Text("Camera",
+                      style: GoogleFonts.roboto(
+                          fontSize: 24, color: Colors.blueGrey[900]))),
+              SizedBox(
+                width: 20,
+              ),
+              FlatButton.icon(
+                  onPressed: () {
+                    takePhoto(ImageSource.gallery);
+                  },
+                  icon:
+                      Icon(Icons.image, color: Colors.blueGrey[900], size: 34),
+                  label: Text("Gallery",
+                      style: GoogleFonts.roboto(
+                          fontSize: 24, color: Colors.blueGrey[900]))),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
