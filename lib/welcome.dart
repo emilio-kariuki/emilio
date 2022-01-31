14fe3e047d637ef07c4f69b699ccd2c9d4255ea5
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-31 20:25:14 +0300

    January 31, 2022 8:25 PM

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Welcome extends StatefulWidget {
   Welcome({Key? key}) : super(key: key);
 
@@ -11,8 +10,28 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final mediaQuery = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: mediaQuery.height,
+            width: mediaQuery.width,
+            child: Image.asset(
+              'assets/images/welcome.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            right: 10,
+            bottom: 20,
+            child: CircleAvatar(
+              backgroundColor: Colors.blueGrey[800],
+              child: Icon(Icons.arrow_right,color: Colors.white,size: mediaQuery.width * 0.03),
+              radius: 30,
+            ),)
+        ],
+      ),
     );
   }
 }
