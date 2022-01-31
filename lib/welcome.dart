d379371b569a825868c7c4475d2513bf285b5c62
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-31 20:17:34 +0300

    January 31, 2022 8:17 PM

---
+++ lib/welcome.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Welcome extends StatefulWidget {
+  Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
