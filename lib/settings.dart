1d73da1dec5b92753377322d8d36c3942bb8738a
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-01 11:46:48 +0300

    February 1, 2022 11:46 AM

--- lib/settings.dart
+++ lib/settings.dart
@@ -72,8 +72,8 @@ class _AccountsState extends State<Accounts> {
                         borderRadius: BorderRadius.circular(20)),
                     elevation: 4,
                     child: Container(
-
-                      decoration: new BoxDecoration (
+                      // ignore: unnecessary_new
+                      decoration:  BoxDecoration (
                       color: Colors.red
                       ),
                       child: ListTile(
